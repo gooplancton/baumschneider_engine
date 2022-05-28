415e5e890114dc1b3771aa9380824a8fe02baabb
gooplancton <gooplancton@outlook.com>
2022-05-28 18:26:27 +0200

    opening book functionality

--- BaumschneiderCore/src/OpeningBook.jl
+++ BaumschneiderCore/src/OpeningBook.jl
@@ -4,6 +4,7 @@ module OpeningBook
 using ..BoardRepresentation
 using ..MoveRepresentation
 using ..MoveRepresentationUtils
+using Random
 
 
 function opening_uci_to_move(gs::GameState, uci::String)::Move
@@ -76,6 +77,45 @@ function opening_uci_to_move(gs::GameState, uci::String)::Move
             to_square_idx,
             captured_piece
         )
+    end
+
+end
+
+
+function generate_opening_book_from_file(fname::String, min_moves::Int = 4)::Vector{Vector{String}}
+    file = open(fname)
+    openings = []
+
+    while !eof(file)
+        opening_str = readine(file)
+        opening_vec = split(opening_str, " ")
+        if length(opening_vec) >= min_moves
+            push!(openings, opening_vec)
+        end
+    end
+
+    close(file)
+    return openings
+end
+
+
+function sample_opening_book(openings::Vector{Vector{String}}, current::Vector{String})::Union{String, Nothing}
+    matching_openings = filter(
+        op->all(op[i] == cur_mov for (i, cur_mov) in enumerate(current)),
+        openings
+    )
+
+    if length(matching_openings) == 0
+        return nothing
+    end
+
+    opening = shuffle(matching_openings)[1]
+    next_move = opening[length(current) + 1]
+
+    if next_move == "*"
+        return nothing
+    else
+        return next_move
     end
 
 end
